a782b54ad336877d66844a7b1bab1ec79616fd59
JamesJJ <James_Hooper@htc.com>
2014-12-25 16:19:40 +0800

    cosmetic

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,11 @@
 default[:yum_localrepo][:createrepo_package] = 'createrepo'
 default[:yum_localrepo][:yumd_path] = '/etc/yum.repos.d'
+default[:yum_localrepo][:templates_cookbook] = nil
+default[:yum_localrepo][:files_cookbook] = nil
 default[:yum_localrepo][:repos] = [ 
   {
   name: 'yum_local_repo',
-  path: '/opt/yum_local_repo',
+  path: '/opt/yum_localrepo',
   enabled: 1,
   gpgcheck: 0,
   protect: 1,
