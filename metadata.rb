0df3fb941152b00362d4825cb4143b8e1458bb42
JamesJJ <James_Hooper@htc.com>
2014-12-25 21:38:25 +0800

    v0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'github@jamesjj.net'
 license          'MIT'
 description      'Installs/Configures yum-localrepo'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
