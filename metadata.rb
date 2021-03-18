12d04e809991a0889ef7f7168fcafc12795d665c
JamesJJ <jj@fcg.fyi>
2021-03-18 21:17:15 +0800

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'yum-localrepo'
-maintainer       'github@jamesjj.net'
-maintainer_email 'github@jamesjj.net'
+maintainer       'JamesJJ'
+maintainer_email 'JamesJJ@users.noreply.github.com'
 license          'MIT'
 description      'Installs/Configures yum-localrepo'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
